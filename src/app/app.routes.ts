e625544a2dabfdc44578cc37c7854132a3282424
dunmer0 <popa.catalin88@gmail.com>
2024-06-13 19:57:16 +0300

    Fixed reset password.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { AdminComponent } from './admin/admin.component';
 import { adminGuard } from './auth/guard/auth-guard/admin.guard';
 import { AdminReparationsComponent } from './admin/admin-reparations/admin-reparations.component';
 import { AdminUserApproveComponent } from './admin/admin-user-approve/admin-user-approve.component';
+import {ResetPasswordComponent} from "./auth/reset-password/reset-password.component";
 
 export const routes: Routes = [
   {path: 'service', canActivate:[authGuard] , component:MechanicComponent},
   {path: 'admin', canActivate:[adminGuard], component: AdminComponent},
   {path:'admin/reparations', canActivate:[adminGuard], component:AdminReparationsComponent},
-  {path:'admin/user-management', canActivate:[adminGuard], component:AdminUserApproveComponent}
+  {path:'admin/user-management', canActivate:[adminGuard], component:AdminUserApproveComponent},
+  {path: 'account/reset-password', component: ResetPasswordComponent},
 
 ];
 
